eebe17bb5fb84f34d3a0ac84528950e3036d9735
Daniela Duarte <daduarte@gmail.com>
2022-12-06 21:52:27 -0300

    Se agrega función toggle y favicon más ajustes finales en estructura y forma

--- assets/jb/main.js
+++ assets/jb/main.js
@@ -1,15 +1,20 @@
-$(document).scroll(function(){
+$(function () {
+    //Tooltip asociado a RRSS
+    $('[data-bs-toggle="tooltip"]').tooltip ();
+
+    //Evento asociado al botón enviar    
+    $('#enviar').click(function() {
+        alert("El formulario fue enviado correctamente");
+    });
+
+    //Evento asociado a card
+    $(".card-title").click(function() {
+        $(".card-text").toggle();
+    });
+});
+
+$(document).scroll(function() {
     const y= $("html").scrollTop();
 
     y > 300 ? $("nav").addClass("nav-black"): $("nav").removeClass("nav-black")
-})
-
-$(function () {
-
-    $('[data-bs-toggle="tooltip"]').tooltip ();
-
-    $('#enviar').click(function() {
-        alert ("El formulario fue enviado correctamente")
-
-    })
-    });
+});
